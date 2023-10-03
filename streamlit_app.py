100cd6250b56b6379693319d97c90b52b5e75864
Abe <74078480+code-with-abe@users.noreply.github.com>
2023-10-03 00:23:41 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain.llms import OpenAI
 st.set_page_config(page_title="🦜🔗 Quickstart App")
-st.title('🦜🔗 Quickstart App')
+st.title('Abe Quickstart App')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
